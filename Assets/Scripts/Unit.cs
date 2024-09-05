ffa0c5681f06c2f15b0c395f079b7b2bb5f3ec16
paulcuongnguyen <cuong091087@gmail.com>
2024-09-05 22:37:34 +0700

    Initial commit

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    [SerializeField] private Vector3 targetPosition;
+
+    
+    private void Update()
+    {
+        float stoppingDistance = .1f;
+        if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
+        {
+            Vector3 moveDirection = (targetPosition - transform.position).normalized;
+            float moveSpeed = 4f;
+            transform.position += moveDirection * moveSpeed *Time.deltaTime;
+        }
+        
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Move(MouseWorld.GetPosition());
+        }
+    }
+
+    private void Move(Vector3 targetPosition)
+    {
+        this.targetPosition = targetPosition;
+    }
+    
+}
